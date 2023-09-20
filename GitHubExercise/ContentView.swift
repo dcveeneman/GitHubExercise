138849f36adaf84d09f5044af92e78ec22572172
David Veeneman <davidv@veeneman.com>
2023-09-20 14:05:31 -0700

    Initial Commit

---
+++ GitHubExercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubExercise
+//
+//  Created by David Veeneman on 9/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by David Veeneman on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
