ee453143c6e3d3e81f05734eb2a34b436e4f5644
David Veeneman <davidv@veeneman.com>
2023-09-20 17:31:37 -0700

    Version 3.0 - blue

--- GitHubExercise/ContentView.swift
+++ GitHubExercise/ContentView.swift
@@ -11,16 +11,16 @@ struct ContentView: View {
     var body: some View {
         VStack {
             
-            Image(systemName: "2.circle")
+            Image(systemName: "3.circle")
                 .resizable()
                 .scaledToFit()
 
-            Text("Version 2.0")
+            Text("Version 3.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
         }
         .padding()
-        .foregroundColor(.green)
+        .foregroundColor(.blue)
     }
 }
 
